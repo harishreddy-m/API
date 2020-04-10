8dc8bc4f53c687eb2421adf866b5eef32a8ebe15
harish-mi <harish.mallu@movingimage.com>
2020-04-10 20:08:18 +0200

    pulled changes

--- server.js
+++ server.js
@@ -5,60 +5,32 @@ const app = express();
 const { redis, config, scraper } = require('./routes/instances');
 const { keys } = config;
 
-<<<<<<< HEAD
-const execAll = () => {
-    scraper.getWorldometers.getCountries(keys, redis);
-    scraper.getWorldometers.getYesterday(keys, redis);
-    scraper.getAll(keys, redis);
-    scraper.getStates(keys, redis);
-    scraper.jhuLocations.jhudataV2(keys, redis);
-    scraper.historical.historicalV2(keys, redis);
-=======
-const execAll = async () => {
-	await Promise.all([
-		scraper.getWorldometerPage(keys, redis),
-		scraper.getStates(keys, redis),
-		scraper.jhuLocations.jhudataV2(keys, redis),
-		scraper.historical.historicalV2(keys, redis),
-		scraper.historical.getHistoricalUSADataV2(keys, redis)
-	]);
-	app.emit('scrapper_finished');
->>>>>>> a7a5b6926aaf313b2b3b8aee7c9073bbd0de4f4e
+const execAll = async() => {
+    await Promise.all([
+        scraper.getWorldometerPage(keys, redis),
+        scraper.getStates(keys, redis),
+        scraper.jhuLocations.jhudataV2(keys, redis),
+        scraper.historical.historicalV2(keys, redis),
+        scraper.historical.getHistoricalUSADataV2(keys, redis)
+    ]);
+    app.emit('scrapper_finished');
 };
 
 execAll();
 setInterval(execAll, config.interval);
 
-<<<<<<< HEAD
-app.get('/', async(request, response) => {
-    response.redirect('docs');
-});
-
-const listener = app.listen(process.env.PORT || config.port, () => {
-    console.log(`Your app is listening on port ${listener.address().port}`);
-});
-
-=======
 app.use(cors());
-app.get('/', async (request, response) => response.redirect('https://github.com/novelcovid/api'));
 
-const listener = app.listen(config.port, () =>
-	console.log(`Your app is listening on port ${listener.address().port}`)
+const listener = app.listen(process.env.PORT || config.port, () =>
+    console.log(`Your app is listening on port ${listener.address().port}`)
 );
-
-app.get('/invite', async (req, res) =>
-	res.redirect('https://discordapp.com/oauth2/authorize?client_id=685268214435020809&scope=bot&permissions=537250880')
-);
-
-app.get('/support', async (req, res) => res.redirect('https://discord.gg/EvbMshU'));
->>>>>>> a7a5b6926aaf313b2b3b8aee7c9073bbd0de4f4e
 
 app.use('/public', express.static('assets'));
-app.use('/docs',
+app.use('/',
     swaggerUi.serve,
     swaggerUi.setup(null, {
         explorer: true,
-        customSiteTitle: 'NovelCOVID 19 API',
+        customSiteTitle: 'COVID 19 API',
         customfavIcon: '/public/virus.png',
         customCssUrl: '/public/apidocs/custom.css',
         swaggerOptions: {
